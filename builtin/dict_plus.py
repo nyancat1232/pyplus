e7ba9999560be36e4cc5f0d2a52bb308dfe12c57
Lua Jeon <nyancat2323.1232@gmail.com>
2024-03-30 22:29:52 +0900

    add example

--- builtin/dict_plus.py
+++ builtin/dict_plus.py
@@ -1,5 +1,30 @@
 
 def inverse_dict(dict_current:dict)->dict:
+    '''
+    Inverse dictionary between key and value, and if there are duplicate values, then use list.
+    
+    Parameters
+    ----------
+    dict_current : dict
+        Dictionary what you want to apply.
+    
+    See Also
+    --------
+    (sa_description)
+    
+    Returns
+    --------
+    dict
+        dictionary.
+    
+    Examples
+    --------
+    
+    >>> import pyplus.builtin as bp
+    >>> vv = {'apple':'fruit','banana':'fruit','cat':'animal'}
+    >>> bp.inverse_dict(vv)
+    {'fruit': ['apple', 'banana'], 'animal': ['cat']}
+    '''
     dict_inverse = dict()
     for key in dict_current:
         if dict_current[key] in dict_inverse:
