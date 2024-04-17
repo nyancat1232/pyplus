b50ae6dbcaacedea0be0e0d07e54aba35fa983d9
Lua Jeon <nyancat2323.1232@gmail.com>
2024-04-17 16:08:45 +0900

    date

--- sql/oopgplus.py
+++ sql/oopgplus.py
@@ -45,7 +45,7 @@ def _convert_pgsql_type_to_pandas_type(pgtype:str):
         case 'double precision':
             return pd.Float64Dtype()
         case 'date':
-            return 'object'
+            return pd.DatetimeTZDtype('ns',tz=ZoneInfo('UTC'))
         case 'timestamp without time zone':
             return 'datetime64[ns]'
         case 'timestamp with time zone':
