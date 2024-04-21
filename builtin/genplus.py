4276788c0faecff0f7dfd4c9ebafca596f3e7e99
Lua Jeon <nyancat2323.1232@gmail.com>
2024-04-21 23:51:04 +0900

    doc

--- builtin/genplus.py
+++ builtin/genplus.py
@@ -24,6 +24,24 @@ def select_yielder(gen:Generator[tuple[Any,str],Any,None],msg:str):
     >>> aa = select_yielder('Rick: banaba',test_yield('Rick'))
     >>> aa
     1
+    
+    Recommend using for a private method
+    >>> import pandas as pd
+    >>> import pyplus.builtin as bp
+    >>> 
+    >>> class Test:
+    >>>     def _private_method(self):
+    >>>         ...
+    >>>         yield df.copy(), 'first return'
+    >>>         ...
+    >>>         yield df.copy(), 'second return'
+    >>>         ...
+    >>>
+    >>>     def first_method(self):
+    >>>         return bp.select_yielder(self._private_method(),'first return') 
+    >>>
+    >>>     def second_method(self):
+    >>>         return bp.select_yielder(self._private_method(),'second return') 
     '''
     for ret,current_msg in gen:
         if current_msg == msg:
