1647016bc454156eb8d30d3c21c7372189e98efd
Lua Jeon <nyancat2323.1232@gmail.com>
2024-05-02 22:42:56 +0900

    change arg

--- streamlit/streamlit_plus.py
+++ streamlit/streamlit_plus.py
@@ -68,7 +68,7 @@ class TabsPlus:
     >>> with tabs[0]:
     >>>     ...
     '''
-    def __init__(self,connection:Literal['tab','column']='tab',tabs: list[str]=[]):
+    def __init__(self,connection:Literal['tab','column']='tab',*tabs:str):
         '''
         (description)
         
