58e6cbe8bd34849f2974c9163ab2a91f662889cb
Lua Jeon <luajeon506@gmail.com>
2024-08-15 13:19:01 +0900

    new: naming

--- builtin/__init__.py
+++ builtin/__init__.py
@@ -1,4 +1,4 @@
 from pyplus.builtin.case_calc import CaseCalc
 from pyplus.builtin.dict_plus import inverse_dict
 from pyplus.builtin.genplus import select_yielder,pass_sender,pass_senders_parallel
-from pyplus.builtin.checkpoint_function import CheckPointFunctionV2,dec_checkpoint_function_v2
+from pyplus.builtin.checkpoint_function import CheckPointFunctionV2 as CheckPointFunction ,dec_checkpoint_function_v2 as dec_checkpoint_function
